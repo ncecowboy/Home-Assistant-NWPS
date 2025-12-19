6deb831d8b3a73584841d8fdf44385e87bc50b0d
ncecowboy <ncecowboy1@gmail.com>
2025-12-19 11:48:16 -0800

    Update config_flow.py with correct NWPS config flow including CONF_PARAMETERS with cv.multi_select

--- custom_components/nwps_water/config_flow.py
+++ custom_components/nwps_water/config_flow.py
@@ -1,14 +1,19 @@
-"""Config flow for NWPS Water component."""
+"""Config flow for NWPS Water integration."""
+from __future__ import annotations
+
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
-from homeassistant.const import CONF_NAME
 
-from .const import DOMAIN, CONF_PARAMETERS, DEFAULT_PARAMETERS
+from .const import (
+    DOMAIN, 
+    CONF_STATION, 
+    CONF_PARAMETERS, 
+    AVAILABLE_PARAMETERS, 
+    DEFAULT_SCAN_INTERVAL
+)
 
-
-class NWPSWaterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class NWPSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for NWPS Water."""
 
     VERSION = 1
@@ -18,68 +23,30 @@ class NWPSWaterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            # Validate the input
-            if not user_input.get(CONF_NAME):
-                errors[CONF_NAME] = "required"
+            # 1. Check for duplicates
+            await self.async_set_unique_id(user_input[CONF_STATION])
+            self._abort_if_unique_id_configured()
 
-            if not errors:
-                return self.async_create_entry(
-                    title=user_input[CONF_NAME],
-                    data=user_input,
-                )
-
-        # Build the schema
-        schema = vol.Schema({
-            vol.Required(CONF_NAME): cv.string,
-            vol.Optional(CONF_PARAMETERS, default=DEFAULT_PARAMETERS): cv.multi_select({
-                param: param for param in DEFAULT_PARAMETERS
-            }),
-        })
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=schema,
-            errors=errors,
-        )
-
-    @staticmethod
-    def async_get_options_flow(config_entry):
-        """Get the options flow for this config entry."""
-        return NWPSWaterOptionsFlow(config_entry)
-
-
-class NWPSWaterOptionsFlow(config_entries.OptionsFlow):
-    """Handle options for NWPS Water."""
-
-    def __init__(self, config_entry):
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        """Handle the initial options step."""
-        if user_input is not None:
             return self.async_create_entry(
-                title="",
-                data=user_input,
+                title=f"NWPS {user_input[CONF_STATION]}",
+                data={CONF_STATION: user_input[CONF_STATION]},
+                options={
+                    CONF_PARAMETERS: user_input.get(CONF_PARAMETERS, list(AVAILABLE_PARAMETERS.keys())),
+                    "scan_interval": user_input.get("scan_interval", DEFAULT_SCAN_INTERVAL),
+                },
             )
 
-        # Get current settings
-        current_parameters = self.config_entry.options.get(
-            CONF_PARAMETERS, 
-            self.config_entry.data.get(CONF_PARAMETERS, DEFAULT_PARAMETERS)
+        # 2. Define schema with parameter selection
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_STATION): str,
+                vol.Optional(CONF_PARAMETERS, default=list(AVAILABLE_PARAMETERS.keys())): cv.multi_select(AVAILABLE_PARAMETERS),
+                vol.Optional("scan_interval", default=DEFAULT_SCAN_INTERVAL): int,
+            }
         )
 
-        # Build the schema
-        schema = vol.Schema({
-            vol.Optional(
-                CONF_PARAMETERS, 
-                default=current_parameters
-            ): cv.multi_select({
-                param: param for param in DEFAULT_PARAMETERS
-            }),
-        })
-
         return self.async_show_form(
-            step_id="init",
-            data_schema=schema,
+            step_id="user", 
+            data_schema=schema, 
+            errors=errors
         )
